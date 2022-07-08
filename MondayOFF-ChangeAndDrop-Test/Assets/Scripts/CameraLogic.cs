596969bcdfbc1f40de430cd27d5f28f615a46fe7
tero <dotnr3028@naver.com>
2022-07-09 04:11:48 +0900

    추가 업데이트
    1. 카메라가 공을 따라가는 알고리즘 추가
    2. 맵 변경

---
+++ MondayOFF-ChangeAndDrop-Test/Assets/Scripts/CameraLogic.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLogic : MonoBehaviour
+{
+    [SerializeField] private BallGenerator ballGenerator;
+    [SerializeField] private Vector3 cameraPosition;
+    [SerializeField] private float cameraMoveSpeed = 1f;
+    [SerializeField] private Vector3 cameraOffset ;
+
+
+    void FixedUpdate()
+    {
+        if(ballGenerator.generatedBallList.Count > 0)
+        {
+            cameraPosition = ballGenerator.generatedBallList[0].transform.position;
+
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, cameraPosition.y, transform.position.z) + cameraOffset, cameraMoveSpeed * Time.deltaTime);
+        }
+        
+    }
+}
